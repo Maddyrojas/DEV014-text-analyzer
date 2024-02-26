40433de9ba0e8f3650ab1fc5804afdf19423ca4b
Madelyn Rojas Moya <mmrojasmoya@gmail.com>
2024-02-26 00:34:16 -0600

    Updates

---
+++ analyzer.js
@@ -0,0 +1,43 @@
+const analyzer = {  
+  getWordCount: (text) => {
+    //recuento de palabras
+    var cantPalabras = 0;
+    while(text!=""){
+
+    }
+    return cantPalabras;
+  },
+  getCharacterCount: (text) => {
+    //recuento de caracteres
+    var cantCaracteres = 0;
+    return cantCaracteres;
+  },
+  getCharacterCountExcludingSpaces: (text) => {
+    //recuento de caracteres excluyendo espacios y signos de puntuación
+    var cantCaracteresSinSignos = 0;
+    return cantCaracteresSinSignos;
+  },
+  getAverageWordLength: (text) => {    
+    //longitud media
+    var longitudMedia = 0;
+    return longitudMedia;
+  },
+  getNumberCount: (text) => {
+    //cantidad de números
+    var cantNumeros = 0;
+    return cantNumeros;
+  },
+  getNumberSum: (text) => {
+    //suma de todos los números
+    var sumaNumeros = 0;
+    return sumaNumeros;
+  },
+};
+
+export default analyzer;
+
+
+function limpiar (){
+var text="";
+return text;
+}
