74c4c508de1fb4ddcb1c5b88b27194954bca6c2c
Madelyn Rojas Moya <mmrojasmoya@gmail.com>
2024-02-29 11:07:15 -0600

    termine mis test

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,200 @@
 const analyzer = {  
+  //recuento de palabras
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const palabra=text.toString();
+    const aregloPalabras = palabra.split(" ");
+    let contadorPalabras = 0;
+    for (let i = 0; i < aregloPalabras.length; i++) {
+      if (aregloPalabras[i]!==" ") { 
+        contadorPalabras = contadorPalabras+1;
+      }
+    }
+    return contadorPalabras;
   },
+  //recuento de caracteres
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
+  //recuento de caracteres excluyendo espacios y signos de puntuación
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const simbolosDePuntuacion = ",.!?;:(){}[]$&\"'¡¿`-—_…";
+    let palabra='';
+    for (let i = 0; i < text.length; i++) {
+      if (text[i]!==" ") { //si es diferente a vacio
+        if (!simbolosDePuntuacion.includes(text[i]) ) {//si la constante no conincide en alguno de sus valores con el caracter
+          palabra+= text[i];
+        }
+      }
+    }
+    return palabra.length;
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  
+  /*
+  longitud 
+  media
+  */
+  getAverageWordLength: (text) => {
+    const aregloPalabras = [];
+    let palabraActual='';
+    let todasPalabras='';
+    let promedio = 0;
+    if (text!=="") {
+      for (let i = 0; i < text.length; i++) {
+        if (text[i]!==" ") { //si es diferente a vacio quiero que concatene
+          palabraActual+= text[i];
+          if (i===text.length-1) {// si ademas de ser diferente a vacio es el ultimo valor quiero que lo agregue.
+            aregloPalabras.push(palabraActual);
+            todasPalabras+=palabraActual;
+          }
+        }
+        else{
+          if (palabraActual.length>0) {// si valor es espacio y ademas hay palabra con longitud mayor a 0
+            aregloPalabras.push(palabraActual);
+            todasPalabras+=palabraActual;
+            palabraActual='';
+          }
+        }
+      }
+      promedio=(todasPalabras.length/aregloPalabras.length).toFixed(2);
+      return +promedio;
+    }
   },
+  //cantidad de números
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const cantidadNumero = [];
+    const numero = "1234567890";
+    const punto = ".";
+    let unionNumero=null;
+    for (let i = 0; i < text.length; i++) {
+      if (numero.includes(text[i]) ) {//si el caracter es numero
+        if (text[i-1]=== " " || i===0){ //si el anterio era espacio [o] si numero actual es el primero
+          if (text[i+1]=== " " || i===text.length-1) { 
+            /*si el caracter es numero
+              si es el primero
+              si el siguiente caracter es espacio [o] si el actual carcter es el ultimo*/
+            cantidadNumero.push(text[i]);
+          } 
+          else {
+            if (numero.includes(text[i+1]) || punto.includes(text[i+1])) {
+              /*si el caracter es numero
+                si es el primero
+                si no es el ultimo
+                si el siguiente es numero [o] punto*/
+              unionNumero+= text[i];
+            }
+          }
+          
+        }
+        else{
+          /*Si el caracter es numero
+            si NO es primero*/
+          if (unionNumero!==null) {
+            if (text[i+1]=== " " || i===text.length-1) {
+              /*
+                Si el caracter es numero
+                si NO primero
+                palabra no es null
+                es ultimo*/
+              unionNumero+= text[i];
+              cantidadNumero.push(unionNumero);
+              unionNumero=null;
+            }
+            else{
+              if (numero.includes(text[i+1]) || punto.includes(text[i+1])) {
+                /*Si el caracter es numero
+                  si NO es primero
+                  palabra no es null
+                  No es ultimo
+                  siguiente es numero [o] punto*/
+                unionNumero+= text[i];
+              }
+              else{
+                unionNumero=null;
+              }
+            }
+          }//if no null
+        }
+      }//if es numero
+      else{
+        if (punto.includes(text[i]) && unionNumero!==null) {//si es un punto y ya hay algo antes
+          if (i!==text.length-1 && text[i+1]!==" ") {//si no es ultimo
+            if (!punto.includes(unionNumero) && numero.includes(text[i+1])){ 
+              /* Es punto
+                hay numeros antes
+                no es ultimo
+                no hay otro punto
+                y el siguiente es numero*/
+              unionNumero+= text[i];
+            }
+          }
+          else{// creo que esto puede ser opcional dependiendo de cada uno
+            unionNumero+= text[i];
+            cantidadNumero.push(unionNumero);
+            unionNumero=null;
+          }
+        }
+      }
+       
+    }   
+    return cantidadNumero.length;
   },
+  //suma de todos los números
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let sumaTotal = 0;
+    const numero = "1234567890";
+    const punto = ".";
+    let unionNumero='';
+    for (let i = 0; i < text.length; i++) {
+      if (numero.includes(text[i]) ) {//si el caracter es numero
+        if (text[i-1]=== " " || i===0){ //si el anterio era espacio [o] si numero actual es el primero
+          if (text[i+1]=== " " || i===text.length-1) { 
+            sumaTotal += Number(text[i]);
+          } 
+          else {
+            if (numero.includes(text[i+1]) || punto.includes(text[i+1])) {
+              unionNumero+= text[i];
+            }
+          }
+        }
+        else{
+          if (unionNumero!=='') {
+            if (text[i+1]=== " " || i===text.length-1) {
+              unionNumero+= ''+text[i];
+              sumaTotal += Number(unionNumero);
+              unionNumero='';
+            }
+            else{
+              if (numero.includes(text[i+1]) || punto.includes(text[i+1])) {
+                unionNumero+= ''+text[i];
+              }
+              else{
+                unionNumero='';
+              }
+            }
+          }//if no null
+        }
+      }//if es numero
+      else{
+        if (punto.includes(text[i]) && unionNumero!=='') {//si es un punto y ya hay algo antes
+          if (i!==text.length-1 ){
+            if(text[i+1]!==" ") {//si no es ultimo
+              if (!punto.includes(unionNumero) && numero.includes(text[i+1])){ 
+                unionNumero+= ''+text[i];
+              }
+            }
+            else{
+              sumaTotal += Number(unionNumero);
+              unionNumero='';
+            }
+          }
+          else{
+            sumaTotal += Number(unionNumero);
+            unionNumero='';
+          }
+        }
+      }
+    }//for 
+    return sumaTotal;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import analyzer from './analyzer.js';
 
+
+const areaTexto = document.querySelector("textarea");
+const palabra = document.querySelector('li[data-testid=word-count]');
+const caracteres = document.querySelector('li[data-testid=character-count]');
+const caracteresSinEspacios = document.querySelector('li[data-testid=character-no-spaces-count]');
+const numeros = document.querySelector('li[data-testid=number-count]');
+const suma = document.querySelector('li[data-testid=number-sum]');
+const longitudMedia = document.querySelector('li[data-testid=word-length-average]');
+const button = document.getElementById("reset-button");
+
+areaTexto.addEventListener("input", function(){
+  palabra.innerHTML = 'Cantidad Palabras: '+ analyzer.getWordCount(areaTexto.value);
+  caracteres.innerHTML = 'Cantidad Caracteres: ' + analyzer.getCharacterCount(areaTexto.value);
+  caracteresSinEspacios.innerHTML = 'Cantidad Caracteres sin Espacio: ' + analyzer.getCharacterCountExcludingSpaces(areaTexto.value);
+  numeros.innerHTML = 'Cantidad Numeros: ' + analyzer.getNumberCount(areaTexto.value);
+  suma.innerHTML = 'Suma de Numeros: ' + analyzer.getNumberSum(areaTexto.value);
+  longitudMedia.innerHTML = 'Longitud Media: ' + analyzer.getAverageWordLength(areaTexto.value);
+});
+
+button.addEventListener('click', function(){
+  areaTexto.value= "";
+  palabra.innerHTML = 'Cantidad Palabras: 0';
+  caracteres.innerHTML = 'Cantidad Caracteres: 0';
+  caracteresSinEspacios.innerHTML = 'Cantidad Caracteres sin Espacio: 0';
+  numeros.innerHTML = 'Cantidad Numeros: 0';
+  suma.innerHTML = 'Suma de Numeros: 0';
+  longitudMedia.innerHTML = 'Longitud Media: 0';
+})
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//acá debes escuchar eventos del DOM, invocar los métodos del objeto analyzer 
+//según sea necesario y actualizar el resultado en la UI (interfaz de usuaria).
